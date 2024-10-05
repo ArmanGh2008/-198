016010127a9f6c544f3eb15df8a31ff809608e91
ArmanGh2008 <armangharibjanyan137@gmail.com>
2024-10-05 12:54:33 +0400

    Delete Naxagtayin2 directory

--- Naxagtayin2/python.py
+++
@@ -1,6 +0,0 @@
-def is_valid_triangle(a, b, c):
-    # Check the triangle inequality conditions
-    if a + b > c and a + c > b and b + c > a:
-        return "This is a valid triangle"
-    else:
-        return "This is not a valid triangle"
